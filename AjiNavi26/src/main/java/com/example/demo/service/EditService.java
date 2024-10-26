b5f4fe706ea034c9f7255bf3dfc70f577955a6c4
kogaryotaro <ryotaroran531@gmail.com>
2024-10-26 16:07:32 +0900

    実際のDB接続(削除)

---
+++ AjiNavi26/src/main/java/com/example/demo/service/EditService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entitiy.Review;
+
+public interface EditService {
+
+	void edit(Review review);
+}
---
+++ AjiNavi26/src/main/java/com/example/demo/service/RemoveService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entitiy.Review;
+
+public interface RemoveService {
+
+	void remove(Review review);
+}
